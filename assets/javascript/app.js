e9649ba1e8b23c69a7189969aba4569e0aebfa86
Dan Smith <daniel.smith8795@gmail.com>
2019-07-17 21:44:35 -0500

    added in test data to firebase, working on pulling down route info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,53 @@ var routenumber = 0;
 var destination = '';
 var frequency = '';
 var stops = '';
-var nextarrivaltime = '';
-var timeremaining = '';
+var startTime = '';
+var endTime = '';
 
+
+$("#Submit").on("click", function () {
+    event.preventDefault();
+    routenumber = $("#routenum").val().trim();
+    destination = $("#destination").val().trim();
+    frequency = $("#frequency").val().trim();
+    stops = $("#stops").val().trim();
+    startTime = $("#start").val().trim();
+    endTime = $("#end").val().trim();
+    console.log(startTime);
+    database.ref().push({
+        routenumber: routenumber,
+        destination: destination,
+        frequency: frequency,
+        stops: stops,
+        startTime: startTime,
+    });
+});
+database.ref().on("child_added", function (snapshot) {
+    var sv = snapshot.val();
+    console.log(snapshot);
+    console.log(sv.routenumber);
+    console.log(sv.destination);
+    console.log(sv.frequency);
+    console.log(sv.stops);
+    createRow(snapshot.val());
+
+
+}, function (errorObject) {
+    console.log('Errors Handled: ' + errorObject.code);
+});
+
+function createRow(time) {
+    //calculating the time of each stop and counting time left from current time
+    var timesofstops = [];
+    var nextarrivaltime;
+    var timeremaining;
+    var row = $("<tr>");
+    //pulling info from firebase to fill all of the fields
+    row.append($("<td>")).text(time.routenumber);
+    row.append($("<td>")).text(time.destination);
+    row.append($("<td>")).text(time.frequency);
+    row.append($("<td>")).text(time.stops);
+    row.append($("<td>")).text(nextarrivaltime);
+    row.append($("<td>")).text(timeremaining);
+    $(".table").append(row);
+}
